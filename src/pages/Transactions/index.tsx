00eaa85b1a402caed4d57253c6b798e7247bc73a
Mateus Rodrigues <mateusjaed@gmail.com>
2023-03-20 16:00:06 -0300

    first commit

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,42 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./sytles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+      
+      <TransactionsContainer>
+        <SearchForm />
+
+          <TransactionsTable>
+            <tbody>
+              <tr>
+                <td width="50%" >Desenvolvimento de site</td>
+                <td>
+                  <PriceHighlight variant="income">
+                    R$ 12.000,00
+                  </PriceHighlight>
+                </td>
+                <td>Venda</td>
+                <td>20/03/2023</td>
+              </tr>
+              <tr>
+                <td width="50%" >Rolê</td>
+                <td>
+                  <PriceHighlight variant="outcome">
+                    - R$ 59,00
+                  </PriceHighlight>
+                </td>
+                <td>Lifestyle</td>
+                <td>18/03/2023</td>
+              </tr>
+            </tbody>
+          </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
