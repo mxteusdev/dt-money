c27b6286160037c30f6100674cd2f8dd7476669e
Mateus Rodrigues <mateusjaed@gmail.com>
2023-03-22 14:36:18 -0300

    "app finished"

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,8 +3,26 @@ import { SearchFormContainer } from './styles'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
+import { memo } from 'react'
+
+/*
+? Por que um componente renderiza?
+! Hooks changed (mudou estado, contexto, reducer).
+! Props changed (mudou propriedades).
+! Parent rerendered (componente pai renderizou).
+
+? Qual o fluxo de renderização?
+! 1. O React reacria o HTML da interface daquele componente.
+! 2. Compara a versão do HTML recriada com a versão anterior.
+! 3. Se mudou alguma coisa, eçe reescreve o HTML na tela.
+
+? Memo: 
+! 0. Hooks changed, Props changed (deep comparison).
+! 0.1: Comparar a versão anterior dos hooks e props
+! 0.2: SE mudou algo, ele vai permitir a nova renderização.
+*/
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -12,8 +30,13 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+function SearchFormComponent() {
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
 
   const {
     register,
@@ -42,3 +65,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
